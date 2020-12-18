ca3e457da0fb34d3226445e69b871436a0cc0bb7
TGab99 <55789282+TGab99@users.noreply.github.com>
2020-12-18 15:02:21 +0100

    Multiparadigm

--- email_masking/src/api.js
+++ email_masking/src/api.js
@@ -1,13 +1,14 @@
-
 const maskEmail = (email = '') => {
-    const [name, domain] = email.split('@');
+    const name = email.substring(0, email.lastIndexOf('@'));
+    const domain = email.substring(email.lastIndexOf('@'));
     const { length: len } = name;
     const maskedName = name[0] + '*' + name[len - 1];
     return maskedName + '@' + domain;
 };
 
 const maskEmail2 = (email = '') =>{
-    const [name, domain] = email.split('@');
+    const name = email.substring(0, email.lastIndexOf('@'));
+    const domain = email.substring(email.lastIndexOf('@'));
     const { length: len } = name;
     const maskedName = name[0] + '*' + name[len];
     return maskedName + '@' + domain;
